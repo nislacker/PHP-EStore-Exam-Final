bfe1077f04525f885ec6209171cc1f3ffd295de6
Nislacker <nislacker@gmail.com>
2019-03-12 13:48:14 +0200

    change interface to yellow color

--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -5,8 +5,7 @@
     <div class="footer-bottom">
         <div class="container">
             <div class="row">
-                <p class="pull-left">Copyright © 2015</p>
-                <p class="pull-right">Курс PHP Start</p>
+                <p style="text-align: center;">EStore © 2019</p>
             </div>
         </div>
     </div>
